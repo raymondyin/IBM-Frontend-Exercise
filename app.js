8f6495056b03426f7fd22c268ea650bdd8fd3be5
itpanda <itpanda123@gmail.com>
2018-04-09 23:59:26 -0400

    making Flickr API request, extracting data and rendering data into template

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 
 var request = require('request');
 var express = require('express');
+
 var app = express();
+
 app.set('view engine', 'ejs');
 
 var PORT = 4321;
+
+var flickrEndPoint = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07&nojsoncallback=1&format=json';
 
 app.get('/', function(req, res){
     console.log('request hitting root page...');
@@ -13,13 +17,11 @@ app.get('/', function(req, res){
         if (!error && response.statusCode == 200) {
             // console.log(body);
             // res.send(body);
-            res.render('mainpage')
+            var data = JSON.parse(body);
+            res.render("mainpage", {data: data});
         }
     } );
 })
-
-var flickrEndPoint = 'https://api.flickr.com/services/rest/?format=json&method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07';
-
 
 
 app.listen(PORT, function(){
