240c78de245096956ea080dc83a9af9a47e77ea9
itpanda <itpanda123@gmail.com>
2018-04-09 22:28:38 -0400

    Adding basic project structure, testing node server and Flickr API connection

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+
+var request = require('request');
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
 
 var PORT = 4321;
 
-app.get('/', function(request, response){
-    response.send('this is a test');
+app.get('/', function(req, res){
+    console.log('request hitting root page...');
+    
+    request(flickrEndPoint, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // console.log(body);
+            // res.send(body);
+            res.render('mainpage')
+        }
+    } );
 })
+
+var flickrEndPoint = 'https://api.flickr.com/services/rest/?format=json&method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=24662369@N07';
+
+
 
 app.listen(PORT, function(){
     console.log('Listening at port: ' + PORT);
-})
+});
